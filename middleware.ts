264dbd5874a40ca25f0819acaa4e7d96627b7b50
augustoribeiro1 <ac.ribeiro@autocomtecnologia.com.br>
2026-01-04 20:54:13 -0300

    Allow /api/track without auth

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 export { default } from 'next-auth/middleware';
-
 export const config = {
   matcher: [
-    '/((?!api/auth|auth|_next/static|_next/image|favicon.ico|r/|c/).*)',
+    '/((?!api/auth|api/track|auth|_next/static|_next/image|favicon.ico|r/|c/).*)',
+    //            ^^^^^^^^^ ADICIONADO
   ],
 };
